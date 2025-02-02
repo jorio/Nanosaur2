4b9966b3fa746088e2ad60565045cbebc2019674
Iliyas Jorio <iliyas@jor.io>
2025-02-02 10:54:59 +0100

    stb_image.h v2.30

--- Source/System/stb_image.c
+++ Source/System/stb_image.c
@@ -1,8 +1,10 @@
-#include "game.h"		// AllocPtr, ReallocPtr, SafeDisposePtr
+#include "game.h"
 
 #define STB_IMAGE_IMPLEMENTATION
+#define STBI_NO_STDIO
 #define STBI_ONLY_JPEG
 #define STBI_ONLY_PNG
+#define STBI_ASSERT GAME_ASSERT
 #define STBI_MALLOC AllocPtr
 #define STBI_REALLOC ReallocPtr
 #define STBI_FREE SafeDisposePtr

ccb975d7d8a4fe3150c37b7a45854ef421a0b192
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-03 21:16:15 -0400

    Port Mines.c to Swift

---
+++ Source/Items/Mines.swift
@@ -0,0 +1,485 @@
+// Mines.swift - Port of Mines.c to Swift
+
+private let airMineScale: Float = 1.2
+
+// MARK: - Add air mine
+
+@c @implementation
+public func AddAirMine(_ itemPtr: UnsafeMutablePointer<TerrainItemEntryType>!, _ x: Float, _ z: Float) -> UInt8 {
+    let typeB: Int32
+    let typeM: Int32
+    let typeC: Int32
+    var chainOff: Float = 0
+
+    var h = Int(itemPtr.pointee.parm.0)
+    if h != 0 {
+        h = 10 - h
+        if h < 0 {
+            h = 0
+        }
+    }
+
+    switch gLevelNum {
+    case Int16(LevelNum.adventure1.rawValue), Int16(LevelNum.flag2.rawValue), Int16(LevelNum.battle1.rawValue):
+        typeB = Int32(LEVEL1_ObjType_AirMine_Base)
+        typeM = Int32(LEVEL1_ObjType_AirMine_Mine)
+        typeC = Int32(LEVEL1_ObjType_AirMine_Chain)
+        if h == 0 {
+            chainOff = RandomFloat() * (600.0 * airMineScale)
+        } else {
+            chainOff = Float(h) * (800.0 / 10.0)
+        }
+
+    case Int16(LevelNum.adventure2.rawValue):
+        typeB = Int32(LEVEL2_ObjType_AirMine_Base)
+        typeM = Int32(LEVEL2_ObjType_AirMine_Mine)
+        typeC = Int32(LEVEL2_ObjType_AirMine_Chain)
+        if h == 0 {
+            chainOff = RandomFloat() * (1400.0 * airMineScale)
+        } else {
+            chainOff = Float(h) * (2300.0 / 10.0)
+        }
+
+    case Int16(LevelNum.adventure3.rawValue), Int16(LevelNum.race1.rawValue), Int16(LevelNum.flag1.rawValue):
+        typeB = Int32(LEVEL3_ObjType_AirMine_Base)
+        typeM = Int32(LEVEL3_ObjType_AirMine_Mine)
+        typeC = Int32(LEVEL3_ObjType_AirMine_Chain)
+        if h == 0 {
+            chainOff = RandomFloat() * (1400.0 * airMineScale)
+        } else {
+            chainOff = Float(h) * (2300.0 / 10.0)
+        }
+
+    default:
+        SwFatal("AddAirMine: no mines here yet, call Brian!")
+        return 0
+    }
+
+    // MAKE BASE
+
+    var def = NewObjectDefinitionType()
+    def.group = UInt8(MODEL_GROUP_LEVELSPECIFIC)
+    def.type = UInt8(typeB)
+    def.scale = airMineScale
+    def.coord.x = x
+    def.coord.z = z
+    def.flags = gAutoFadeStatusBits
+    def.slot = Int16(SLOT_OF_DUMB) - 200
+    def.moveCall = cMoveAirMine
+    def.rot = 0
+
+    def.coord.y = GetMinTerrainY(x, z, Int16(def.group), Int16(def.type), def.scale)
+
+    let base = MakeNewDisplayGroupObject(&def)!
+
+    base.pointee.TerrainItemPtr = itemPtr // keep ptr to item list
+
+    // SET COLLISION STUFF
+
+    base.pointee.CType = UInt32(CTYPE_MISC)
+    base.pointee.CBits = UInt32(CBITS_ALLSOLID)
+    CreateCollisionBoxFromBoundingBox(base, 1, 1)
+
+    // MAKE CHAIN
+
+    def.type = UInt8(typeC)
+    def.coord.y -= chainOff
+    def.slot += 1
+    def.moveCall = nil
+    def.rot = RandomFloat() * SwPI2
+    def.flags = gAutoFadeStatusBits | UInt32(STATUS_BIT_CLIPALPHA6 | STATUS_BIT_DOUBLESIDED)
+    let chain = MakeNewDisplayGroupObject(&def)!
+
+    chain.pointee.SpecialF.0 = RandomFloat() * SwPI2 // WobbleX
+
+    base.pointee.ChainNode = chain
+    chain.pointee.ChainHead = base
+
+    // MAKE MINE
+
+    def.type = UInt8(typeM)
+    def.slot += 1
+    def.flags = gAutoFadeStatusBits
+    let mine = MakeNewDisplayGroupObject(&def)!
+
+    // SET COLLISION STUFF
+
+    mine.pointee.Damage = 1.0
+
+    mine.pointee.CType = UInt32(CTYPE_MISC | CTYPE_WEAPONTEST | CTYPE_TRIGGER)
+    mine.pointee.CBits = UInt32(CBITS_ALLSOLID)
+    CreateCollisionBoxFromBoundingBox(mine, 0.5, 0.9)
+
+    mine.pointee.TriggerCallback = cDoTrig_AirMine
+    mine.pointee.HitByWeaponHandler = cAirMineHitByWeaponCallback
+
+    chain.pointee.ChainNode = mine
+    mine.pointee.ChainHead = chain
+
+    // CALL THE MOVE FUNCTION ONCE TO ALIGN ALL THE PARTS
+
+    cMoveAirMine(base)
+
+    return 1 // item was added
+}
+
+// MARK: - Move air mine
+
+private let mineOff = OGLPoint3D(x: 0, y: 1000, z: 0)
+private let mineOff2 = OGLPoint3D(x: 0, y: 2000, z: 0)
+private let lightOff = OGLPoint3D(x: 0, y: 1100, z: 0)
+private let lightOff2 = OGLPoint3D(x: 0, y: 2100, z: 0)
+
+private let cMoveAirMine: @convention(c) (UnsafeMutablePointer<ObjNode>?) -> Void = { baseOpt in
+    let base = baseOpt!
+    let chain = base.pointee.ChainNode!
+    let mine = chain.pointee.ChainNode!
+    var origin = OGLPoint3D()
+    var m = OGLMatrix4x4()
+    var m2 = OGLMatrix4x4()
+
+    // SEE IF GONE
+
+    if TrackTerrainItem(base) != 0 {
+        DeleteObject(base)
+        return
+    }
+
+    // MAKE CHAIN WOBBLE
+
+    chain.pointee.SpecialF.0 += gFramesPerSecondFrac * 1.1 // WobbleX
+    chain.pointee.Rot.x = sin(chain.pointee.SpecialF.0) * 0.15
+
+    // CALC MATRIX TO ROTATE CHAIN AROUND THE HING POINT
+
+    origin.x = base.pointee.Coord.x // set coord to swivel about
+    origin.y = base.pointee.Coord.y + (40.0 * airMineScale)
+    origin.z = base.pointee.Coord.z
+
+    OGLMatrix4x4_SetRotateAboutPoint(&m, &origin, chain.pointee.Rot.x, chain.pointee.Rot.y, 0)
+
+    OGLMatrix4x4_SetScale(&m2, chain.pointee.Scale.x, chain.pointee.Scale.y, chain.pointee.Scale.z)
+    setMatValue(&m2, M03, chain.pointee.Coord.x)
+    setMatValue(&m2, M13, chain.pointee.Coord.y)
+    setMatValue(&m2, M23, chain.pointee.Coord.z)
+
+    OGLMatrix4x4_Multiply(&m2, &m, &chain.pointee.BaseTransformMatrix)
+
+    SetObjectTransformMatrix(chain)
+
+    // PUT MINE ON END OF CHAIN
+
+    mine.pointee.Rot.x = chain.pointee.Rot.x // match x rot
+
+    var mineOffVar: OGLPoint3D
+    switch gLevelNum {
+    case Int16(LevelNum.adventure1.rawValue), Int16(LevelNum.flag2.rawValue), Int16(LevelNum.battle1.rawValue):
+        mineOffVar = mineOff // calc coord of mine @ end of chain
+
+    default:
+        mineOffVar = mineOff2
+    }
+    OGLPoint3D_Transform(&mineOffVar, &chain.pointee.BaseTransformMatrix, &mine.pointee.Coord)
+
+    mine.updateTransforms()
+    CalcObjectBoxFromNode(mine)
+
+    // UPDATE LIGHT ON MINE
+
+    // CALC COORD OF LIGHT
+
+    var lightOffVar: OGLPoint3D
+    switch gLevelNum {
+    case Int16(LevelNum.adventure1.rawValue), Int16(LevelNum.flag2.rawValue), Int16(LevelNum.battle1.rawValue):
+        lightOffVar = lightOff
+
+    default:
+        lightOffVar = lightOff2
+    }
+    OGLPoint3D_Transform(&lightOffVar, &chain.pointee.BaseTransformMatrix, &origin)
+
+    // MAKE NEW SPARKLE
+
+    if sparklesBase(mine)[0] == -1 {
+        let i = GetFreeSparkle(mine) // make new sparkle
+        sparklesBase(mine)[0] = i
+        if i != -1 {
+            let sparkle = GetSparkleSlot(Int32(i))!
+            sparkle.pointee.flags = UInt32(SPARKLE_FLAG_OMNIDIRECTIONAL | SPARKLE_FLAG_FLICKER)
+            sparkle.pointee.where = origin
+
+            sparkle.pointee.color.r = 1
+            sparkle.pointee.color.g = 1
+            sparkle.pointee.color.b = 1
+            sparkle.pointee.color.a = 1
+
+            sparkle.pointee.scale = 100.0
+            sparkle.pointee.separation = 300.0
+
+            sparkle.pointee.textureNum = Int16(PARTICLE_SObjType_RedGlint)
+        }
+    }
+
+    // UPDATE EXISTING SPARKLE
+
+    else {
+        let i = sparklesBase(mine)[0]
+        GetSparkleSlot(Int32(i))!.pointee.where = origin
+    }
+}
+
+@inline(__always) private func sparklesBase(_ n: UnsafeMutablePointer<ObjNode>) -> UnsafeMutablePointer<Int16> {
+    UnsafeMutableRawPointer(n.pointer(to: \.Sparkles)!).assumingMemoryBound(to: Int16.self)
+}
+
+// MARK: -
+
+// MARK: - Trigger callback: air mine
+
+// Returns TRUE if want to handle hit as a solid
+private let cDoTrig_AirMine: @convention(c) (UnsafeMutablePointer<ObjNode>?, UnsafeMutablePointer<ObjNode>?) -> UInt8 = { mineOpt, playerOpt in
+    let mine = mineOpt!
+    let player = playerOpt!
+    let playerNum = player.pointee.PlayerNum
+
+    let pi = GetPlayerInfoEntry(Int32(playerNum))!
+
+    // DOES PLAYER HAVE SHIELD?
+
+    if pi.pointee.shieldPower > 0.0 {
+        HitPlayerShield(Int16(playerNum), MAX_SHIELD_POWER, 2.5, 1) // completely drain shield
+    }
+
+    // NO SHIELD, SO KABOOM
+
+    else if gGamePrefs.kiddieMode == 0 { // dont hurt in kiddie mode
+        _ = PlayerSmackedIntoObject(player, mine, Int16(PlayerDeathType.deathDive.rawValue))
+    }
+
+    explodeAirMine(mine)
+
+    return 0
+}
+
+// MARK: - Air mine hit by weapon callback
+
+// Returns true if object should stop bullet.
+private let cAirMineHitByWeaponCallback: @convention(c) (UnsafeMutablePointer<ObjNode>?, UnsafeMutablePointer<ObjNode>?, UnsafeMutablePointer<OGLPoint3D>?, UnsafeMutablePointer<OGLVector3D>?) -> UInt8 = { _, mineOpt, _, _ in
+    explodeAirMine(mineOpt!)
+
+    return 1
+}
+
+// MARK: - Explode air mine
+
+private func explodeAirMine(_ mine: UnsafeMutablePointer<ObjNode>) {
+    let chain = mine.pointee.ChainHead!
+    let base = chain.pointee.ChainHead!
+
+    // FIRST MAKE SPARKS
+
+    gNewParticleGroupDef.magicNum = 0
+    gNewParticleGroupDef.type = UInt8(ParticleType.fallingSparks.rawValue)
+    gNewParticleGroupDef.flags = UInt32(PARTICLE_FLAGS_BOUNCE)
+    gNewParticleGroupDef.gravity = 900
+    gNewParticleGroupDef.magnetism = 0
+    gNewParticleGroupDef.baseScale = 10
+    gNewParticleGroupDef.decayRate = 0.4
+    gNewParticleGroupDef.fadeRate = 0.7
+    gNewParticleGroupDef.particleTextureNum = UInt8(PARTICLE_SObjType_BlueSpark)
+    gNewParticleGroupDef.srcBlend = GL_SRC_ALPHA
+    gNewParticleGroupDef.dstBlend = GL_ONE
+    let pg = NewParticleGroup(&gNewParticleGroupDef)
+    if pg != -1 {
+        let x = mine.pointee.Coord.x
+        let y = mine.pointee.Coord.y
+        let z = mine.pointee.Coord.z
+
+        for _ in 0..<70 {
+            var d = OGLVector3D()
+            d.x = RandomFloat2() * 800.0
+            d.y = RandomFloat2() * 800.0
+            d.z = RandomFloat2() * 800.0
+
+            var pt = OGLPoint3D()
+            pt.x = x + d.x * 0.05
+            pt.y = y + d.y * 0.05
+            pt.z = z + d.z * 0.05
+
+            var newParticleDef = NewParticleDefType()
+            newParticleDef.groupNum = pg
+            newParticleDef.scale = RandomFloat() + 1.5
+            newParticleDef.rotZ = 0
+            newParticleDef.rotDZ = 0
+            newParticleDef.alpha = Float(FULL_ALPHA) + (RandomFloat() * 0.3)
+
+            withUnsafeMutablePointer(to: &pt) { ptPtr in
+                withUnsafeMutablePointer(to: &d) { dPtr in
+                    newParticleDef.where = ptPtr
+                    newParticleDef.delta = dPtr
+                    AddParticleToGroup(&newParticleDef)
+                }
+            }
+        }
+    }
+
+    // MAKE FLARE BALLS
+
+    for _ in 0..<10 {
+        var def = NewObjectDefinitionType()
+        def.genre = UInt8(CUSTOM_GENRE)
+        def.slot = Int16(SLOT_OF_DUMB) + 20
+        def.moveCall = cMoveAirMineFlareBall
+        def.flags = UInt32(STATUS_BIT_DONTCULL)
+        def.scale = 1
+
+        let flare = MakeNewObject(&def)!
+
+        // SET RANDOM TRAJECTORY
+
+        let dx = RandomFloat2()
+        let dy = RandomFloat2() + 0.5
+        let dz = RandomFloat2()
+
+        FastNormalizeVector(dx, dy, dz, &flare.pointee.Delta)
+
+        // AND COORD
+
+        flare.pointee.Coord.x = mine.pointee.Coord.x + flare.pointee.Delta.x * 50.0
+        flare.pointee.Coord.y = mine.pointee.Coord.y + flare.pointee.Delta.y * 50.0
+        flare.pointee.Coord.z = mine.pointee.Coord.z + flare.pointee.Delta.z * 50.0
+
+        flare.pointee.Delta.x *= 800.0
+        flare.pointee.Delta.y *= 800.0
+        flare.pointee.Delta.z *= 800.0
+
+        flare.pointee.Health = 1.2 + RandomFloat() * 0.3
+
+        // MAKE NEW SPARKLE
+
+        let i = GetFreeSparkle(flare)
+        sparklesBase(flare)[0] = i
+        if i != -1 {
+            let sparkle = GetSparkleSlot(Int32(i))!
+            sparkle.pointee.flags = UInt32(SPARKLE_FLAG_OMNIDIRECTIONAL)
+            sparkle.pointee.where = mine.pointee.Coord
+
+            sparkle.pointee.color.r = 1
+            sparkle.pointee.color.g = 1
+            sparkle.pointee.color.b = 1
+            sparkle.pointee.color.a = 1
+
+            sparkle.pointee.scale = 120.0
+            sparkle.pointee.separation = 0.0
+
+            sparkle.pointee.textureNum = Int16(PARTICLE_SObjType_YellowGlint)
+        }
+    }
+
+    // EXPLODE GEOMETRY
+
+    ExplodeGeometry(mine, 600, .fromOrigin, 1, 1.0)
+
+    PlayEffect3D(Int16(EFFECT_MINEEXPLODE), &gCoord)
+
+    // DELETE MINE & CLEANUP LINKAGES
+
+    base.pointee.TerrainItemPtr = nil // don't come back
+
+    DeleteObject(mine)
+    DeleteObject(chain)
+
+    base.pointee.ChainNode = nil
+
+    base.pointee.MoveCall = MoveStaticObject
+}
+
+// MARK: - Move air mine flare ball
+
+private let cMoveAirMineFlareBall: @convention(c) (UnsafeMutablePointer<ObjNode>?) -> Void = { flareOpt in
+    let flare = flareOpt!
+    let fps = gFramesPerSecondFrac
+
+    flare.pointee.Health -= fps
+    if flare.pointee.Health <= 0.0 {
+        DeleteObject(flare)
+        return
+    }
+
+    flare.getInfo()
+
+    gDelta.y -= 500.0 * fps
+
+    gCoord.x += gDelta.x * fps
+    gCoord.y += gDelta.y * fps
+    gCoord.z += gDelta.z * fps
+
+    let i = sparklesBase(flare)[0]
+    if i != -1 {
+        GetSparkleSlot(Int32(i))!.pointee.where = gCoord
+    }
+
+    flare.pointee.Delta = gDelta
+    flare.pointee.Coord = gCoord
+
+    // UpdateObject(flare)
+
+    // UPDATE SPARKLE TRAIL
+
+    flare.pointee.ParticleTimer -= fps
+    if flare.pointee.ParticleTimer <= 0.0 {
+        flare.pointee.ParticleTimer += 0.03 // reset timer
+
+        var particleGroup = flare.pointee.ParticleGroup
+        let magicNum = flare.pointee.ParticleMagicNum
+
+        if particleGroup == -1 || VerifyParticleGroupMagicNum(particleGroup, magicNum) == 0 {
+            let newMagicNum = MyRandomLong() // generate a random magic num
+            flare.pointee.ParticleMagicNum = newMagicNum
+
+            gNewParticleGroupDef.magicNum = newMagicNum
+            gNewParticleGroupDef.type = UInt8(ParticleType.fallingSparks.rawValue)
+            gNewParticleGroupDef.flags = UInt32(PARTICLE_FLAGS_DONTCHECKGROUND)
+            gNewParticleGroupDef.gravity = -200
+            gNewParticleGroupDef.magnetism = 0
+            gNewParticleGroupDef.baseScale = 10.0
+            gNewParticleGroupDef.decayRate = 0
+            gNewParticleGroupDef.fadeRate = 0.6
+            gNewParticleGroupDef.particleTextureNum = UInt8(PARTICLE_SObjType_RedSpark)
+            gNewParticleGroupDef.srcBlend = GL_SRC_ALPHA
+            gNewParticleGroupDef.dstBlend = GL_ONE
+            particleGroup = NewParticleGroup(&gNewParticleGroupDef)
+            flare.pointee.ParticleGroup = particleGroup
+        }
+
+        if particleGroup != -1 {
+            for _ in 0..<4 {
+                var d = OGLVector3D()
+                d.x = RandomFloat2() * 30.0
+                d.y = RandomFloat2() * 30.0
+                d.z = RandomFloat2() * 30.0
+
+                var p = gCoord
+
+                var newParticleDef = NewParticleDefType()
+                newParticleDef.groupNum = particleGroup
+                newParticleDef.scale = RandomFloat() + 1.0
+                newParticleDef.rotZ = RandomFloat() * SwPI2
+                newParticleDef.rotDZ = RandomFloat2()
+                newParticleDef.alpha = 1.0
+
+                let stop: Bool = withUnsafeMutablePointer(to: &p) { pPtr in
+                    withUnsafeMutablePointer(to: &d) { dPtr in
+                        newParticleDef.where = pPtr
+                        newParticleDef.delta = dPtr
+                        return AddParticleToGroup(&newParticleDef) != 0
+                    }
+                }
+                if stop {
+                    flare.pointee.ParticleGroup = -1
+                    break
+                }
+            }
+        }
+    }
+}

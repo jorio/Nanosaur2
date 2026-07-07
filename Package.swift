4521f000a33c79728e64f65ba7d297c5d789c70a
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-07 09:29:15 -0400

    Add Swift package

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "Nanosaur2",
+    platforms: [
+        .macOS(.v26)
+    ],
+    products: [
+        .library(name: "BG3DFile", type: .static, targets: ["BG3DFile"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-binary-parsing", from: "0.0.2")
+    ],
+    targets: [
+        .target(
+            name: "BG3DFile",
+            dependencies: [
+                .product(name: "BinaryParsing", package: "swift-binary-parsing")
+            ]
+        ),
+        .testTarget(
+            name: "BG3DFileTests",
+            dependencies: ["BG3DFile"],
+            resources: [
+                .copy("Fixtures")
+            ]
+        )
+    ]
+)
